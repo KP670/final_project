a5ffedff6958a875cba2bbde231a6223d666ef78
KPNMC2004 <143018629+KPNMC2004@users.noreply.github.com>
2023-11-21 03:27:40 +1000

    Added PHP

---
+++ src/php/database.php
@@ -0,0 +1,15 @@
+<?php 
+    define($DB_HOST, "sql113.infinityfree.com");
+    define($DB_USER, "if0_35369670");
+    define($DB_PASS, "slLNfC9tOg");
+    define($DB_NAME, "if0_35369670_dishes");
+    
+    // Connect to new instance of DB
+    $conn = new mysqli($DB_HOST, $DB_NAME, $DB_PASS, $DB_NAME);
+
+    //Exit upon connection error
+    if ($conn->connect_error) {
+        die("Connection Error". $conn->connect_error);
+    } 
+
+    echo "HELLO WORLD!";
--- src/php/dishes_menu.php
+++ src/php/dishes_menu.php
@@ -1,11 +1,16 @@
+<?php include 'database.php'?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dishes Menu</title>
 </head>
 <body>
-    <iframe src="php/database.php" frameborder="1"></iframe>
+    <main>
+        <?php echo 'hello world'?>
+        <p>helloworld</p>
+    </main>
 </body>
 </html>
