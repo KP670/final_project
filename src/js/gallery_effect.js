03babb33f0416a32e7729ce164913728767aa830
KPNMC2004 <143018629+KPNMC2004@users.noreply.github.com>
2023-12-05 09:46:41 +1000

    Various changes and additions

---
+++ src/js/gallery_effect.js
@@ -0,0 +1,13 @@
+const mobileNav = document.getElementById("mobile-nav");
+const navBtn = document.getElementById("nav-button");
+
+navBtn.onclick = function () {
+    navBtn.classList.toggle("zoomed")
+
+    if(mobileNav.style.height == "0%") {
+        mobileNav.style.height = "100%";
+        
+    } else {
+        mobileNav.style.height = "0%";
+    }
+}
